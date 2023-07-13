02024cd0f55c9968a4faad9ce7336efbeec757d7
Md Nazmul Hoque Bhuiyan <sisil8sisil@gmail.com>
2023-07-13 13:11:49 +0600

    fix: ENV vars

--- src/pages/[...slug].tsx
+++ src/pages/[...slug].tsx
@@ -26,7 +26,7 @@ export default function HomePage({
               {" "}
               <b>Next public API URL read on the client: </b>
             </td>
-            <td>{process.env.NEXT_PUBLIC_QW_API_URL}</td>
+            <td>{process.env.NEXT_PUBLIC_API_URL}</td>
           </tr>
 
           <tr>
@@ -51,19 +51,19 @@ export async function getStaticPaths() {
 export const getStaticProps: GetStaticProps = async ({ params }) => {
   const slugString = (params?.slug as string[])?.join("/");
 
-  if (!process.env.QW_API_URL) {
-    throw new Error("process.env.QW_API_URL is not defined");
+  if (!process.env.API_URL) {
+    throw new Error("process.env.API_URL is not defined");
   }
 
   console.log("waiting....");
-  // wait for data to be fetched based on the domain process.env.QW_API_URL
+  // wait for data to be fetched based on the domain process.env.API_URL
   await new Promise((resolve) => setTimeout(resolve, 1 * 2 * 1000)); // 2s delay
   console.log("wait done");
 
   return {
     props: {
       apiReadOnTheServer:
-        process.env.QW_API_URL || `process.env.QW_API_URL (((NOT FOUND)))`,
+        process.env.API_URL || `process.env.API_URL (((NOT FOUND)))`,
       slugString,
     },
     revalidate: 3, // Revalidate every 60 seconds
